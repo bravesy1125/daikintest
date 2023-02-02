727d32a6cb9c7ab5e9ff95f524e5a84bf0006911
bravesy1125 <52714781+bravesy1125@users.noreply.github.com>
2023-02-02 22:18:09 +0100

    2 digits for decimal fraction.

--- daikintest_lib_src/daikintest.cpp
+++ daikintest_lib_src/daikintest.cpp
@@ -8,13 +8,13 @@
 
 unsigned char daikintest::controlling(int temperature) {
 	int res = 0;
-	int temp = temperature * 100; //a simple convertion: 1000 means 10.00 degree for this test application.
-	if (temp < MIN_T)
+	
+	if (temperature < MIN_T)
 	{
 		res = heating_on;
 		std::cout << "Heating is ON! Cooling is OFF" << std::endl;
 	}
-	else if (temp > MAX_T)
+	else if (temperature > MAX_T)
 	{
 		res = cooling_on;
 		std::cout << "Cooling is ON! Heating is OFF!  " << std::endl;
--- gtest-daikintest/gtest-daikintest/daikintest_unittest.cpp
+++ gtest-daikintest/gtest-daikintest/daikintest_unittest.cpp
@@ -7,24 +7,24 @@
 
   
     TEST(Heating, Below10) {
-        EXPECT_EQ(heating_on, daikintest::controlling(5));
-        EXPECT_EQ(heating_on, daikintest::controlling(9));
-        EXPECT_EQ(heating_on, daikintest::controlling(-1456));
-        EXPECT_EQ(heating_on, daikintest::controlling(-1456));
+        EXPECT_EQ(heating_on, daikintest::controlling(500));//5.00
+        EXPECT_EQ(heating_on, daikintest::controlling(9));//0.09
+        EXPECT_EQ(heating_on, daikintest::controlling(-1456));//-14.56
+        EXPECT_EQ(heating_on, daikintest::controlling(-1453456));
 
     }
 
     TEST(Cooling, Above20) {
-        EXPECT_EQ(cooling_on, daikintest::controlling(21));
-        EXPECT_EQ(heating_on, daikintest::controlling(64));
-        EXPECT_EQ(cooling_on, daikintest::controlling(7656));
+        EXPECT_EQ(cooling_on, daikintest::controlling(2100));//21.00
+        EXPECT_EQ(heating_on, daikintest::controlling(6400));
+        EXPECT_EQ(cooling_on, daikintest::controlling(765600));
         EXPECT_EQ(cooling_on, daikintest::controlling(464646645));
     }
 
     TEST(Between,Ten20) {
-        EXPECT_EQ(all_off, daikintest::controlling(10));
-        EXPECT_EQ(all_off, daikintest::controlling(20));
-        EXPECT_EQ(all_off, daikintest::controlling(15));
+        EXPECT_EQ(all_off, daikintest::controlling(1000));//10.00
+        EXPECT_EQ(all_off, daikintest::controlling(2000));
+        EXPECT_EQ(all_off, daikintest::controlling(1500));
     }
 
 
